5c1ac0e4c78ae99e11f847ead260ddf02d608f6b
tcchola <tarikcolic22@gmail.com>
2024-01-10 11:33:14 +0100

    entrance video scene

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,12 +1,18 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$"AudioStreamPlayer".play()
+#	$"VideoStreamPlayer".stream.loop = true
+	
+
+#func _process(delta):
+#	if $VideoStreamPlayer.stream.playback_position >= $VideoStreamPlayer.stream.get_length() - delta:
+#		$VideoStreamPlayer.stream.playback_position = 0 # Reset to start
+#		$VideoStreamPlayer.play()
+
 
 func _on_begin_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/entrance_cutscene.tscn")
 
 
 func _on_quit_pressed() -> void:
@@ -15,3 +21,7 @@ func _on_quit_pressed() -> void:
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+
+
+func _on_check_button_toggled(button_pressed: bool) -> void:
+	$"AudioStreamPlayer".stream_paused = !$"AudioStreamPlayer".stream_paused
